262c71ed0d89603060a5026e2e0ddf446b1fbc72
Salvador Pereira <Ramon.Pereirae@gmail.com>
2022-12-08 15:31:39 -0600

    update depencies step 6

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,7 +14,7 @@ import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgModule
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
